d7396dc8c964bc2b6dfc151e4b08e33d8926d675
Akhlakur Rahman <ssrmm2000@gmail.com>
2025-03-03 01:20:25 +0600

    finalize

--- task.js
+++ task.js
@@ -3,7 +3,7 @@ document.getElementById("discover").addEventListener("click", function () {
 });
 
 document.getElementById("theme-btn").addEventListener("click", function () {
-    const colors = ["beige", "silver", "yellow", "aliceblue", "mediumorchid", "mediumpurple", "palegreen", "paleturquoise", "pink", "plum", "powderblue", "	seashell", "skyblue", "slateblue", "snow", "whitesmoke", "black"];
+    const colors = ["beige", "silver", "yellow", "aliceblue", "mediumorchid", "mediumpurple", "palegreen", "paleturquoise", "pink", "plum", "powderblue", "	seashell", "skyblue", "slateblue", "snow", "whitesmoke", "black", "red", "violet"];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
 });
@@ -37,23 +37,23 @@ document.getElementById("btn-2").addEventListener("click", function (event) {
     const card1 = document.getElementById("cloud").innerText;
 
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
-    act.appendChild(p);
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
+    act.appendChild(para);
   } else {
     document.getElementById("nav-c").innerText = intNavC + 1;
     document.getElementById("task-c").innerText = intTaskC - 1;
 
     const card1 = document.getElementById("cloud").innerText;
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
 
-    act.appendChild(p);
+    act.appendChild(para);
   }
 });
 
@@ -75,23 +75,23 @@ document.getElementById("btn-1").addEventListener("click", function (event) {
     const card1 = document.getElementById("shop").innerText;
 
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
-    act.appendChild(p);
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
+    act.appendChild(para);
   } else {
     document.getElementById("nav-c").innerText = intNavC + 1;
     document.getElementById("task-c").innerText = intTaskC - 1;
 
     const card1 = document.getElementById("shop").innerText;
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
 
-    act.appendChild(p);
+    act.appendChild(para);
   }
 });
 
@@ -113,23 +113,23 @@ document.getElementById("btn-3").addEventListener("click", function (event) {
     const card1 = document.getElementById("swift").innerText;
 
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
-    act.appendChild(p);
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
+    act.appendChild(para);
   } else {
     document.getElementById("nav-c").innerText = intNavC + 1;
     document.getElementById("task-c").innerText = intTaskC - 1;
 
     const card1 = document.getElementById("swift").innerText;
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
 
-    act.appendChild(p);
+    act.appendChild(para);
   }
 });
 
@@ -151,23 +151,23 @@ document.getElementById("btn-4").addEventListener("click", function (event) {
     const card1 = document.getElementById("meta").innerText;
 
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
-    act.appendChild(p);
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
+    act.appendChild(para);
   } else {
     document.getElementById("nav-c").innerText = intNavC + 1;
     document.getElementById("task-c").innerText = intTaskC - 1;
 
     const card1 = document.getElementById("meta").innerText;
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
 
-    act.appendChild(p);
+    act.appendChild(para);
   }
 });
 
@@ -189,23 +189,23 @@ document.getElementById("btn-5").addEventListener("click", function (event) {
     const card1 = document.getElementById("google").innerText;
 
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
-    act.appendChild(p);
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
+    act.appendChild(para);
   } else {
     document.getElementById("nav-c").innerText = intNavC + 1;
     document.getElementById("task-c").innerText = intTaskC - 1;
 
     const card1 = document.getElementById("google").innerText;
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
 
-    act.appendChild(p);
+    act.appendChild(para);
   }
 });
 
@@ -227,23 +227,23 @@ document.getElementById("btn-6").addEventListener("click", function (event) {
     const card1 = document.getElementById("glass").innerText;
 
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
-    act.appendChild(p);
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
+    act.appendChild(para);
   } else {
     document.getElementById("nav-c").innerText = intNavC + 1;
     document.getElementById("task-c").innerText = intTaskC - 1;
 
     const card1 = document.getElementById("glass").innerText;
     const act = document.getElementById("activity");
-    const p = document.createElement("p");
+    const para = document.createElement("p");
     const time = currentTime();
-    p.innerText = `You have completed the task ${card1} at ${time}`;
-    p.classList.add("bg-gray-100", "p-4", "mb-4", "rounded-lg", "font-medium");
+    para.innerText = `You have completed the task ${card1} at ${time}`;
+    para.classList.add( "text-base", "font-normal", "bg-[#F4F7FF]", "p-5", "mb-5", "rounded-lg");
 
-    act.appendChild(p);
+    act.appendChild(para);
   }
 });
 
